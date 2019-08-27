ed87a0a5bdb44e49459fa43ff76cca202485944e
datamoa <irisshowy@gmail.com>
2019-08-26 19:22:37 -0700

    added codes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,39 @@
 // from data.js
 var tableData = data;
-// YOUR CODE HERE!
-var tbody = d3.select("tbody");
+//Add data key and value to the table
+var tableData = d3.select("tbody");
 data.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
-    var row = tbody.append("tr");
+    var row = tableData.append("tr");
     Object.entries(ufoSighting).forEach(function([key, value]) {
-     // console.log(key, value);
-     var cell = tbody.append('td') //Ysis nemelteer oruulsan
+     var cell = tableData.append('td') 
      cell.text(value);
     });
   });
+// Select the button
+var button = d3.select("#button");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#data-input-search");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  
+  var filteredData = data.filter(event => event.datetime === inputValue);
+  // var filteredData = data.filter(event => event.datetime === inputValue).then(test => (console.log('test')));
+  console.log(filteredData)});
+
+
+
+  filteredData = filteredData.forEach(function(filteredSighting) {
+    var row = tableData.append("tr");
+    Object.entries(filteredSighting).forEach(function([key, value]) {
+     var cell = tableData.append('td') 
+     cell.text(value);
+    });
+  });
+
+
